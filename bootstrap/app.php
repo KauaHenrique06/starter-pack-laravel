b6d3659b75f805a4c886ec8ff13314f9105400e0
KauaHenrique06 <kauahcarmo@gmail.com>
2026-04-14 21:59:55 -0300

    <CHORE>[exceptions]: add the custom basics exceptions for the error treatment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Support\ApiResponse;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +22,49 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function(Throwable $e) {
+
+            if($e instanceof AuthenticationException) {
+                return ApiResponse::error(
+                    'Your credentials are invalid!',
+                    401
+                );
+            }
+
+            if($e instanceof AuthorizationException) {
+                return ApiResponse::error(
+                    "You don't have permission for this action!",
+                    403
+                );
+            }
+
+            if($e instanceof ModelNotFoundException || $e instanceof NotFoundHttpException) {
+                return ApiResponse::error(
+                    "Resources not found!",
+                    404
+                );
+            }
+
+            if($e instanceof ValidationException) {
+                return ApiResponse::error(
+                    $e->getMessage(),
+                    422
+                );
+            }
+
+            if($e instanceof MethodNotAllowedHttpException) {
+                return ApiResponse::error(
+                    'This method is invalid!',
+                    405
+                );
+            }
+
+            if($e instanceof Throwable || $e instanceof Exception) {
+                return ApiResponse::error(
+                    "Server internal error!",
+                    500
+                );
+            }
+        });
     })->create();
+
