02189015722f026b193f9725dbce5ab45c465ece
KauaHenrique06 <kauahcarmo@gmail.com>
2026-07-17 19:42:28 -0300

    <CHORE>[scramble]: init scramble configuration

--- routes/api/address.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Address\AddressController;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware('auth:api')->group(function() {
-    Route::get('/', [AddressController::class, 'index']);
-    Route::post('/', [AddressController::class, 'store']);
-});
--- routes/api/index.php
+++ routes/api/index.php
@@ -5,4 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(base_path('routes/api/auth.php'));
 Route::prefix('role')->group(base_path('routes/api/role.php'));
 Route::prefix('permission')->group(base_path('routes/api/permission.php'));
-Route::prefix('address')->group(base_path('routes/api/address.php'));
